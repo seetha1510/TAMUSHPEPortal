a8ecba512cb743e6c2e3df9e6e27c20deccd647b
Yifei Liang <yifei.liang@tamu.edu>
2021-02-11 22:38:58 -0600

    fixing space in between

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,6 +1,6 @@
-class UserProfile < ApplicationRecord 
-        self.primary_key = "user_id" 
-        belongs_to :user
+class UserProfile < ApplicationRecord
+        self.primary_key = "user_id" 
+        belongs_to :user
         has_many :employees
         has_many :employers, through: :employees
 
