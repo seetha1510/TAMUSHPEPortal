0e5d65c071bbe0ee71585f93af43543834438bc6
Seethalakshmi <seetha1510@tamu.edu>
2021-02-22 19:42:56 -0600

    sign out test

---
+++ spec/views/setting_spec.rb
@@ -0,0 +1,51 @@
+require "rails_helper"
+
+RSpec.describe 'Tamu ship ', type: :system do
+    describe 'settings page' do
+        it 'shows the right content' do
+            visit new_account_registration_path
+            fill_in "Email",    with: "yifei.liang@tamu.edu"
+            fill_in "Password", with: "zx453359523"
+            fill_in "Password confirmation", with: "zx453359523"
+            click_on "Sign up"
+            fill_in "Email",    with: "yifei.liang@tamu.edu"
+            fill_in "Password", with: "zx453359523"
+            fill_in "Password confirmation", with: "zx453359523"
+            click_on "Sign up"
+            
+            ## to do test creating new profile page
+            fill_in "Enter First Name", with: "Yifei"
+            fill_in "Enter Last Name",  with: "Liang"
+            fill_in "Enter Facebook URL" ,    with: "facebook.com"
+            fill_in "Enter Instagram URL",  with: "instagram.com"
+            fill_in "Enter Linkedin URL",       with: "linkedin.com"
+            fill_in "Enter Phone Number",   with: "8327589525"
+            fill_in "Enter Graduating Year",    with: "2022"
+            click_on "Create User profile"
+        
+            visit setting_path
+            expect(page).to have_content('Settings')
+            expect(page).to have_content('Signed in as')
+        end
+
+        it 'goes to the right page after sign out' do
+            visit setting_path
+            click_on "Sign out"
+            expect(page).to have_content('Welcome to the memberSHPE Portal!')
+        end
+
+        it 'goes to the right page after delete my account' do
+            visit account_session_path
+            fill_in "Email",    with: "yifei.liang@tamu.edu"
+            fill_in "Password", with: "zx453359523"
+            click_on "Log in"
+            visit setting_path
+            click_on "Cancel my account"
+            # expect(page).to have_content('Are you sure?')
+            a = page.driver.browser.switch_to.alert
+            expect(a.text).to eq("Are you sure?")
+            a.accept
+            expect(page).to have_content('Welcome to the memberSHPE Portal!')
+        end
+    end
+end
