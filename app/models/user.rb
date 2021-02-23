e39011e8324c9f468abd7e9271d67313e32a2964
Seethalakshmi <seetha1510@tamu.edu>
2021-02-22 18:30:54 -0600

    user model update and change for forgot password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     validates_presence_of :user_email
+    validates :admin_status, inclusion: { in: [true, false] }
     self.primary_key = "user_email"
     has_one :user_profile, dependent: :destroy
     scope :get_current_user, ->(current_account) {UserProfile.where(user_email:current_account.email).first}
