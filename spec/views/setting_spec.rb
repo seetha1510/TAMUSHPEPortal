a45b4c5d29e497fa88928f67315478feee6c3021
Seethalakshmi <seetha1510@tamu.edu>
2021-02-23 16:38:00 -0600

    more test on setting page

--- spec/views/setting_spec.rb
+++ spec/views/setting_spec.rb
@@ -45,6 +45,7 @@ RSpec.describe 'Tamu ship ', type: :system do
             click_on "Create User profile"
             visit setting_path
             click_on "Sign Out"
+            expect(page).to have_content('Signed out successfully.')
             expect(page).to have_content('Welcome to the memberSHPE Portal!')
 
         end
@@ -79,6 +80,40 @@ RSpec.describe 'Tamu ship ', type: :system do
             expect(a.text).to eq("Are you sure?")
             a.accept
             expect(page).to have_content('Welcome to the memberSHPE Portal!')
+            expect(page).to have_content('Bye! Your account has been successfully cancelled.')
+        end
+
+        it 'goes to the right page after cancelling delete my account' do
+            visit new_account_registration_path
+            fill_in "Email",    with: "yifei.liang@tamu.edu"
+            fill_in "Password", with: "zx453359523"
+            fill_in "Password confirmation", with: "zx453359523"
+            click_on "Sign up"
+
+            
+            ## to do test creating new profile page
+            fill_in "Enter First Name", with: "Yifei"
+            fill_in "Enter Last Name",  with: "Liang"
+            fill_in "Enter Facebook URL" ,    with: "facebook.com"
+            fill_in "Enter Instagram URL",  with: "instagram.com"
+            fill_in "Enter Linkedin URL",       with: "linkedin.com"
+            fill_in "Enter Phone Number",   with: "8327589525"
+            fill_in "Enter Graduating Year",    with: "2022"
+            click_on "Create User profile"
+            visit setting_path
+            click_on "Sign Out"
+            visit account_session_path
+            fill_in "Email",    with: "yifei.liang@tamu.edu"
+            fill_in "Password", with: "zx453359523"
+            click_on "Log in"
+            visit setting_path
+            click_on "Cancel my account"
+            # expect(page).to have_content('Are you sure?')
+            a = page.driver.browser.switch_to.alert
+            expect(a.text).to eq("Are you sure?")
+            a.decline
+            expect(page).to have_content('Settings')
+            expect(page).to have_content('Signed in as')
         end
     end
 end
