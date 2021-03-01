d248c3f24bc150ae5cb4cd0c193b8bc736f686b0
Yifei Liang <yifei.liang@tamu.edu>
2021-03-01 14:19:57 -0600

    testing branch procetection rule

--- app/models/account.rb
+++ app/models/account.rb
@@ -6,7 +6,7 @@ class Account < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   after_commit :create_in_user_table, on: :create
-  after_commit :delete_in_user_table, on: :destroy
+  after_commit :delete_in_user_table, on: :destroy   
 
   def create_in_user_table
     User.create!(user_email: email, admin_status: false)
