fc9f9746a1655a070547b6be095fb4fe1bc585ce
Yifei Liang <yifei.liang@tamu.edu>
2021-03-02 00:04:57 -0600

    update headless chrome

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,8 +8,14 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
-Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new app, browser: :chrome,
+    options: Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu])
+end
+
+Capybara.javascript_driver = :chrome
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
