07d18909e9d1f19d7cbe214f638eda80244b1ba8
Yifei Liang <yifei.liang@tamu.edu>
2021-03-02 00:12:19 -0600

    update

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,12 +10,15 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 
 
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new app, browser: :chrome,
-    options: Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu])
-end
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w[headless disable-gpu no-sandbox disable-dev-shm-usage] }
+  )
 
-Capybara.javascript_driver = :chrome
+  Capybara::Selenium::Driver.new app,
+                                 browser: :chrome,
+                                 desired_capabilities: capabilities
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
