751c781945436f4fd6e6b55026b93c089f07910a
Yifei Liang <yifei.liang@tamu.edu>
2021-04-04 21:22:36 -0500

    brakeman output and rubocp output

--- app/models/committee.rb
+++ app/models/committee.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Committee < ApplicationRecord
-    has_many :members
-    has_many :user_profiles, through: :member
-    validates :committee_name, presence: true, uniqueness: true
+  has_many :members
+  has_many :user_profiles, through: :member
+  validates :committee_name, presence: true, uniqueness: true
 end
