dc026c9778a35c80a14f15add951ddb064a2cf9b
jonathan343 <jonathangee09@gmail.com>
2021-04-11 00:27:05 -0500

    Rspec fix

--- spec/views/xss_testing_spec.rb
+++ spec/views/xss_testing_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe 'Search bar on XSS attack ', type: :system do
       fill_in 'Enter Last Name',  with: '<script>alert()</script>'
       click_on 'Create User profile'
       expect(page).to have_content('<script>alert()</script>')
-      expect(page).to have_content('Setting')
+      expect(page).to have_content('Settings')
     end
 
     it 'can search' do
@@ -38,7 +38,7 @@ RSpec.describe 'Search bar on XSS attack ', type: :system do
       click_on 'Create User profile'
       expect(page).to have_content('Yifei')
       expect(page).to have_content('Settings')
-      click_link('People', match: :first)
+      click_link('Search', match: :first)
 
       fill_in 'Search', with: '<script>alert()</script>'
       within('form') do
